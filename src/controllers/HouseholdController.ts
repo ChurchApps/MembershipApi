2f2cd549721712c72f85871f0e138acbdc9da769
pranavmalvawala <pranavmalvawala@gmail.com>
2021-01-30 10:19:19 +0530

    Update permission list

--- src/controllers/HouseholdController.ts
+++ src/controllers/HouseholdController.ts
@@ -2,6 +2,7 @@ import { controller, httpPost, httpGet, interfaces, requestParam, httpDelete } f
 import express from "express";
 import { MembershipBaseController } from "./MembershipBaseController"
 import { Household } from "../models"
+import { Permissions } from '../helpers/Permissions'
 
 @controller("/households")
 export class HouseholdController extends MembershipBaseController {
@@ -23,7 +24,7 @@ export class HouseholdController extends MembershipBaseController {
     @httpPost("/")
     public async save(req: express.Request<{}, {}, Household[]>, res: express.Response): Promise<interfaces.IHttpActionResult> {
         return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess("Households", "Edit")) return this.json({}, 401);
+            if (!au.checkAccess(Permissions.households.edit)) return this.json({}, 401);
             else {
                 const promises: Promise<Household>[] = [];
                 req.body.forEach(household => { household.churchId = au.churchId; promises.push(this.repositories.household.save(household)); });
@@ -36,7 +37,7 @@ export class HouseholdController extends MembershipBaseController {
     @httpDelete("/:id")
     public async delete(@requestParam("id") id: number, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
         return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess("Households", "Edit")) return this.json({}, 401);
+            if (!au.checkAccess(Permissions.households.edit)) return this.json({}, 401);
             else await this.repositories.household.delete(au.churchId, id);
         });
     }
