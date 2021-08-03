f04573bd6b5d6c92f109db73d27fbc485f44bfc7
Jared Martin <jared@livechurchsolutions.org>
2021-08-03 10:40:52 -0500

    Remove Form models

--- src/models/Answer.ts
+++
@@ -1,7 +0,0 @@
-export class Answer {
-    public id?: string;
-    public churchId?: string;
-    public formSubmissionId?: string;
-    public questionId?: string;
-    public value?: string;
-}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,4 @@
 export { Group } from './Group';
 export { GroupMember } from './GroupMember';
 export { Household } from './Household';
-export { Answer } from "./Answer";
-export { Question } from "./Question";
-export { Form } from "./Form";
-export { FormSubmission } from "./FormSubmission";
 export { ContactInfo, Name, Person } from '../apiBase/models'
