af5f4e2cb6210fea5a2867dda81bb76433f2cd82
Jeremy Zongker <jeremy@trilitech.com>
2022-12-08 16:36:55 -0600

    Added my groups route

--- src/controllers/GroupController.ts
+++ src/controllers/GroupController.ts
@@ -7,49 +7,56 @@ import { Permissions } from '../helpers/Permissions'
 @controller("/groups")
 export class GroupController extends MembershipBaseController {
 
-    @httpGet("/search")
-    public async search(req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            const campusId = req.query.campusId.toString();
-            const serviceId = req.query.serviceId.toString();
-            const serviceTimeId = req.query.serviceTimeId.toString();
-            return this.repositories.group.convertAllToModel(au.churchId, await this.repositories.group.search(au.churchId, campusId, serviceId, serviceTimeId));
-        });
-    }
+  @httpGet("/search")
+  public async search(req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      const campusId = req.query.campusId.toString();
+      const serviceId = req.query.serviceId.toString();
+      const serviceTimeId = req.query.serviceTimeId.toString();
+      return this.repositories.group.convertAllToModel(au.churchId, await this.repositories.group.search(au.churchId, campusId, serviceId, serviceTimeId));
+    });
+  }
 
-    @httpGet("/:id")
-    public async get(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            return this.repositories.group.convertToModel(au.churchId, await this.repositories.group.load(au.churchId, id));
-        });
-    }
+  @httpGet("/my")
+  public async getMy(req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      return this.repositories.group.convertAllToModel(au.churchId, await this.repositories.group.loadForPerson(au.personId));
+    });
+  }
 
-    @httpGet("/")
-    public async getAll(req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            return this.repositories.group.convertAllToModel(au.churchId, await this.repositories.group.loadAll(au.churchId));
-        });
-    }
+  @httpGet("/:id")
+  public async get(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      return this.repositories.group.convertToModel(au.churchId, await this.repositories.group.load(au.churchId, id));
+    });
+  }
 
-    @httpPost("/")
-    public async save(req: express.Request<{}, {}, Group[]>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.groups.edit)) return this.json({}, 401);
-            else {
-                const promises: Promise<Group>[] = [];
-                req.body.forEach(group => { group.churchId = au.churchId; promises.push(this.repositories.group.save(group)); });
-                const result = await Promise.all(promises);
-                return this.repositories.group.convertAllToModel(au.churchId, result);
-            }
-        });
-    }
+  @httpGet("/")
+  public async getAll(req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      return this.repositories.group.convertAllToModel(au.churchId, await this.repositories.group.loadAll(au.churchId));
+    });
+  }
 
-    @httpDelete("/:id")
-    public async delete(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
-        return this.actionWrapper(req, res, async (au) => {
-            if (!au.checkAccess(Permissions.groups.edit)) return this.json({}, 401);
-            else await this.repositories.group.delete(au.churchId, id);
-        });
-    }
+  @httpPost("/")
+  public async save(req: express.Request<{}, {}, Group[]>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.groups.edit)) return this.json({}, 401);
+      else {
+        const promises: Promise<Group>[] = [];
+        req.body.forEach(group => { group.churchId = au.churchId; promises.push(this.repositories.group.save(group)); });
+        const result = await Promise.all(promises);
+        return this.repositories.group.convertAllToModel(au.churchId, result);
+      }
+    });
+  }
+
+  @httpDelete("/:id")
+  public async delete(@requestParam("id") id: string, req: express.Request<{}, {}, null>, res: express.Response): Promise<interfaces.IHttpActionResult> {
+    return this.actionWrapper(req, res, async (au) => {
+      if (!au.checkAccess(Permissions.groups.edit)) return this.json({}, 401);
+      else await this.repositories.group.delete(au.churchId, id);
+    });
+  }
 
 }
--- src/repositories/GroupRepository.ts
+++ src/repositories/GroupRepository.ts
@@ -6,62 +6,71 @@ import { UniqueIdHelper } from "../helpers";
 @injectable()
 export class GroupRepository {
 
-    public save(group: Group) {
-        return group.id ? this.update(group) : this.create(group);
-    }
+  public save(group: Group) {
+    return group.id ? this.update(group) : this.create(group);
+  }
 
-    private async create(group: Group) {
-        group.id = UniqueIdHelper.shortId();
-        const sql = "INSERT INTO `groups` (id, churchId, categoryName, name, trackAttendance, parentPickup, removed) VALUES (?, ?, ?, ?, ?, ?, 0);";
-        const params = [group.id, group.churchId, group.categoryName, group.name, group.trackAttendance, group.parentPickup];
-        await DB.query(sql, params);
-        return group;
-    }
+  private async create(group: Group) {
+    group.id = UniqueIdHelper.shortId();
+    const sql = "INSERT INTO `groups` (id, churchId, categoryName, name, trackAttendance, parentPickup, removed) VALUES (?, ?, ?, ?, ?, ?, 0);";
+    const params = [group.id, group.churchId, group.categoryName, group.name, group.trackAttendance, group.parentPickup];
+    await DB.query(sql, params);
+    return group;
+  }
 
-    private async update(group: Group) {
-        const sql = "UPDATE `groups` SET churchId=?, categoryName=?, name=?, trackAttendance=?, parentPickup=? WHERE id=? and churchId=?";
-        const params = [group.churchId, group.categoryName, group.name, group.trackAttendance, group.parentPickup, group.id, group.churchId];
-        await DB.query(sql, params);
-        return group;
-    }
+  private async update(group: Group) {
+    const sql = "UPDATE `groups` SET churchId=?, categoryName=?, name=?, trackAttendance=?, parentPickup=? WHERE id=? and churchId=?";
+    const params = [group.churchId, group.categoryName, group.name, group.trackAttendance, group.parentPickup, group.id, group.churchId];
+    await DB.query(sql, params);
+    return group;
+  }
 
-    public delete(churchId: string, id: string) {
-        return DB.query("UPDATE `groups` SET removed=1 WHERE id=? AND churchId=?;", [id, churchId]);
-    }
+  public delete(churchId: string, id: string) {
+    return DB.query("UPDATE `groups` SET removed=1 WHERE id=? AND churchId=?;", [id, churchId]);
+  }
 
-    public load(churchId: string, id: string) {
-        return DB.queryOne("SELECT * FROM `groups` WHERE id=? AND churchId=? AND removed=0;", [id, churchId]);
-    }
+  public load(churchId: string, id: string) {
+    return DB.queryOne("SELECT * FROM `groups` WHERE id=? AND churchId=? AND removed=0;", [id, churchId]);
+  }
 
-    public loadAll(churchId: string) {
-        return DB.query("SELECT *, (SELECT COUNT(*) FROM groupMembers gm WHERE gm.groupId=g.id) AS memberCount FROM `groups` g WHERE churchId=? AND removed=0 ORDER by categoryName, name;", [churchId]);
-    }
+  public loadAll(churchId: string) {
+    return DB.query("SELECT *, (SELECT COUNT(*) FROM groupMembers gm WHERE gm.groupId=g.id) AS memberCount FROM `groups` g WHERE churchId=? AND removed=0 ORDER by categoryName, name;", [churchId]);
+  }
 
-    public loadByIds(churchId: string, ids: string[]) {
-        const sql = "SELECT * FROM `groups` WHERE churchId=? AND removed=0 AND id IN (" + ids.join(",") + ") ORDER by name";
-        return DB.query(sql, [churchId]);
-    }
+  public loadForPerson(personId: string) {
+    const sql = "SELECT distinct g.*"
+      + " FROM groupMembers gm"
+      + " INNER JOIN groups g on g.id=gm.groupId"
+      + " WHERE personId=?"
+      + " ORDER BY name";
+    return DB.query(sql, [personId]);
+  }
 
-    public search(churchId: string, campusId: string, serviceId: string, serviceTimeId: string) {
-        const sql = "SELECT g.id, g.categoryName, g.name"
-            + " FROM `groups` g"
-            + " LEFT OUTER JOIN groupServiceTimes gst on gst.groupId=g.id"
-            + " LEFT OUTER JOIN serviceTimes st on st.id=gst.serviceTimeId"
-            + " LEFT OUTER JOIN services s on s.id=st.serviceId"
-            + " WHERE g.churchId = ? AND (?=0 OR gst.serviceTimeId=?) AND (?=0 OR st.serviceId=?) AND (? = 0 OR s.campusId = ?) and g.removed=0"
-            + " GROUP BY g.id, g.categoryName, g.name ORDER BY g.name";
-        return DB.query(sql, [churchId, serviceTimeId, serviceTimeId, serviceId, serviceId, campusId, campusId]);
-    }
+  public loadByIds(churchId: string, ids: string[]) {
+    const sql = "SELECT * FROM `groups` WHERE churchId=? AND removed=0 AND id IN (" + ids.join(",") + ") ORDER by name";
+    return DB.query(sql, [churchId]);
+  }
 
-    public convertToModel(churchId: string, data: any) {
-        const result: Group = { id: data.id, categoryName: data.categoryName, name: data.name, trackAttendance: data.trackAttendance, parentPickup: data.parentPickup, memberCount: data.memberCount };
-        return result;
-    }
+  public search(churchId: string, campusId: string, serviceId: string, serviceTimeId: string) {
+    const sql = "SELECT g.id, g.categoryName, g.name"
+      + " FROM `groups` g"
+      + " LEFT OUTER JOIN groupServiceTimes gst on gst.groupId=g.id"
+      + " LEFT OUTER JOIN serviceTimes st on st.id=gst.serviceTimeId"
+      + " LEFT OUTER JOIN services s on s.id=st.serviceId"
+      + " WHERE g.churchId = ? AND (?=0 OR gst.serviceTimeId=?) AND (?=0 OR st.serviceId=?) AND (? = 0 OR s.campusId = ?) and g.removed=0"
+      + " GROUP BY g.id, g.categoryName, g.name ORDER BY g.name";
+    return DB.query(sql, [churchId, serviceTimeId, serviceTimeId, serviceId, serviceId, campusId, campusId]);
+  }
 
-    public convertAllToModel(churchId: string, data: any[]) {
-        const result: Group[] = [];
-        data.forEach(d => result.push(this.convertToModel(churchId, d)));
-        return result;
-    }
+  public convertToModel(churchId: string, data: any) {
+    const result: Group = { id: data.id, categoryName: data.categoryName, name: data.name, trackAttendance: data.trackAttendance, parentPickup: data.parentPickup, memberCount: data.memberCount };
+    return result;
+  }
+
+  public convertAllToModel(churchId: string, data: any[]) {
+    const result: Group[] = [];
+    data.forEach(d => result.push(this.convertToModel(churchId, d)));
+    return result;
+  }
 
 }
