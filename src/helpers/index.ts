91524074a098c1ba3a6c5c5c2151d6eb124961a9
Himali Malvawala <86650000+Himali-Malvawala@users.noreply.github.com>
2024-02-05 19:36:48 +0530

    update churchapps/apihelper (#82)

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,7 +5,7 @@ export { ChurchHelper } from "./ChurchHelper";
 export { Environment } from "./Environment";
 export { GeoHelper } from "./GeoHelper";
 export { HubspotHelper } from "./HubspotHelper";
-export * from './Permissions';
+export { Permissions, permissionsList, ApiName, DisplaySection, ContentType, Actions } from './Permissions';
 export { PersonHelper } from "./PersonHelper";
 export { RoleHelper } from "./RoleHelper";
 export { UserHelper } from "./UserHelper";
