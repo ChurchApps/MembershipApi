0ffc45175cc98767b60bca5742d227b5ecb31426
Jeremy Zongker <jeremy@zongker.net>
2024-06-28 21:38:27 -0500

    Updated dependencies

--- lambda.js
+++ lambda.js
@@ -1,20 +1,27 @@
-const { createServer, proxy } = require('aws-serverless-express');
+const serverlessExpress = require('@codegenie/serverless-express')
 const { init } = require('./dist/App');
 const { Pool } = require('@churchapps/apihelper');
 const { Environment } = require('./dist/helpers/Environment');
 
+let cachedServer;
+
 const checkPool = async () => {
   if (!Environment.connectionString) {
-    await Environment.init(process.env.APP_ENV)
+    await Environment.init(process.env.APP_ENV);
     Pool.initPool();
   }
+};
+
+async function setup (event, context) {
+  await checkPool();
+  const app = await init();
+  cachedServer = serverlessExpress({ app })
+  return cachedServer(event, context)
+};
+
+function handler (event, context) {
+  if (cachedServer) return serverlessExpressInstance(event, context)
+  return setup(event, context)
 }
 
-module.exports.universal = function universal(event, context) {
-  checkPool().then(() => {
-    init().then(app => {
-      const server = createServer(app);
-      return proxy(server, event, context);
-    });
-  });
-}
+exports.handler = handler;
