9c0846e312b32612d4ab26fc443d017ab579e707
Jeremy Zongker <jeremy@zongker.net>
2024-06-30 21:11:32 -0500

    Downgraded serverless express

--- lambda.js
+++ lambda.js
@@ -1,25 +1,22 @@
-const serverlessExpress = require('@codegenie/serverless-express')
-const { init } = require('./dist/App');
+const { createServer, proxy } = require('aws-serverless-express');
+const { init } = require('./dist/app');
 const { Pool } = require('@churchapps/apihelper');
 const { Environment } = require('./dist/helpers/Environment');
 
-let cachedServer;
-
 const checkPool = async () => {
   if (!Environment.connectionString) {
-    await Environment.init(process.env.APP_ENV);
+    await Environment.init(process.env.APP_ENV)
     Pool.initPool();
   }
-};
-
-async function setup (event, context) {
-  await checkPool();
-  const app = await init();
-  cachedServer = serverlessExpress({ app })
-  return cachedServer(event, context)
-};
-
-module.exports.universal = function universal(event, context) {
-  if (cachedServer) return cachedServer(event, context)
-  return setup(event, context)
 }
+
+const universal = function universal(event, context) {
+  checkPool().then(() => {
+    init().then(app => {
+      const server = createServer(app);
+      return proxy(server, event, context);
+    });
+  });
+}
+
+module.exports.universal = universal;
