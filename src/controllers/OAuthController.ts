5379c1176faa5e3dd15fe7bdcf5796569869ce4d
Jeremy Zongker <jeremy@zongker.net>
2025-05-30 23:15:08 -0500

    Removed temporary debugging code

--- src/controllers/OAuthController.ts
+++ src/controllers/OAuthController.ts
@@ -82,16 +82,14 @@ export class OAuthController extends MembershipBaseController {
         return this.json({
           access_token: token.accessToken,
           token_type: "Bearer",
-          expires_in: 120, //3600 * 12,
+          expires_in: 3600 * 12,
           created_at: Math.floor(Date.now() / 1000),
           refresh_token: token.refreshToken,
           scope: token.scopes
         });
       } else if (grant_type === "refresh_token") {
         if (!refresh_token) return this.json({ error: "invalid_request" }, 400);
-        console.log("GETTING REFRESH TOKEN");
         const oldToken = await this.repositories.oAuthToken.loadByRefreshToken(refresh_token);
-        console.log("OLD TOKEN", oldToken);
 
 
         if (!oldToken || oldToken.clientId !== client.clientId) return this.json({ error: "invalid_grant" }, 400);
@@ -105,18 +103,15 @@ export class OAuthController extends MembershipBaseController {
           scopes: oldToken.scopes,
           expiresAt: new Date(Date.now() + 60 * 60 * 1000 * 12) // 12 hours,
         };
-        console.log("Saving new token", token);
         await this.repositories.oAuthToken.save(token);
 
-        console.log("Deleting old token")
-        // Delete old token
         await this.repositories.oAuthToken.delete(oldToken.id);
 
         return this.json({
           access_token: token.accessToken,
           token_type: "Bearer",
           created_at: Math.floor(Date.now() / 1000),
-          expires_in: 120, // 3600 * 12,
+          expires_in: 3600 * 12,
           refresh_token: token.refreshToken,
           scope: token.scopes
         });
